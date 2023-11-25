c6ffe19886b937f86654ccae6d69552565c1d9ff
Beancrock <142068316+Beancrock@users.noreply.github.com>
2023-11-25 01:16:52 -0500

    Add files via upload

--- FHgame2.py
+++ FHgame2.py
@@ -4,86 +4,118 @@ import random
 
 # default variables
 
-player_axis_y = 1
-player_axis_x = 3
-
-x1y5z1 = " "
-x1y4z1 = " "
-x1y3z1 = " "
-x1y2z1 = " "
-x1y1z1 = " "
-x2y5z1 = " "
-x2y4z1 = " "
-x2y3z1 = " "
-x2y2z1 = " "
-x2y1z1 = " "
-x3y5z1 = " "
-x3y4z1 = " "
-x3y3z1 = " "
-x3y2z1 = " "
-x3y1z1 = " "
-x4y5z1 = " "
-x4y4z1 = " "
-x4y3z1 = " "
-x4y2z1 = " "
-x4y1z1 = " "
-x5y5z1 = " "
-x5y4z1 = " "
-x5y3z1 = " "
-x5y2z1 = " "
-x5y1z1 = " "
-
-# map display
-
-def map_display():
-    print("[" + x1y5z1 + "][" + x2y5z1 + "][" + x3y5z1 + "][" + x4y5z1 + "][" + x5y5z1 + "]")
-    print("[" + x1y4z1 + "][" + x2y4z1 + "][" + x3y4z1 + "][" + x4y4z1 + "][" + x5y4z1 + "]")
-    print("[" + x1y3z1 + "][" + x2y3z1 + "][" + x3y3z1 + "][" + x4y3z1 + "][" + x5y3z1 + "]")
-    print("[" + x1y2z1 + "][" + x2y2z1 + "][" + x3y2z1 + "][" + x4y2z1 + "][" + x5y2z1 + "]")
-    print("[" + x1y1z1 + "][" + x2y1z1 + "][" + x3y1z1 + "][" + x4y1z1 + "][" + x5y1z1 + "]")
-    print()
+turn = 0
+player_axis_y = -3
+player_axis_x = -3
+player_axis_z = -3
+player_direction = "↑N"
+enemy_axis_y = 3
+enemy_axis_x = 3
+enemy_axis_z = 3
+enemy_direction = "↓S"
 
 # player input
 
-def player_coordinates():
+def player_commands():
     global player_axis_y
     global player_axis_x
-    print("Location: X=[" + str(player_axis_x) + "] Y=[" + str(player_axis_y) + "]")
-    print()
+    global player_axis_z
+    global player_direction
+    global turn
+    turn = turn + 1
     while True:
-        direction = input("(N)orth. (S)outh. (E)ast. (W)est. :")
+        print("Turn #" + str(turn))
+        print("Location= X: " + str(player_axis_x) + " Y: " + str(player_axis_y) + " Z: " + str(player_axis_z) + " D: " + str(player_direction))
         print()
-        if direction == ("n"):
+        player_choice = input("(F)oward. (L)elt. (R)ight. (D)escend. (A)scend. (S)can. (T)orpedo :")
+        print()
+    # turning left
+        if player_choice == "l":
+            print("Your vessel banks toward portside..")
+            print()
+        if player_choice == "l" and player_direction == "↑N":
+            player_direction = "←W"
+            break
+        if player_choice == "l" and player_direction == "↓S":
+            player_direction = "→E"
+            break
+        if player_choice == "l" and player_direction == "→E":
+            player_direction = "↑N"
+            break
+        if player_choice == "l" and player_direction == "←W":
+            player_direction = "↓S"
+            break
+    # turning right
+        if player_choice == "r":
+            print("Your vessel banks toward starboard..")
+            print()
+        if player_choice == "r" and player_direction == "↑N":
+            player_direction = "→E"
+            break
+        if player_choice == "r" and player_direction == "↓S":
+            player_direction = "←W"
+            break
+        if player_choice == "r" and player_direction == "→E":
+            player_direction = "↓S"
+            break
+        if player_choice == "r" and player_direction == "←W":
+            player_direction = "↑N"
+            break
+    # moving
+        if player_choice == "f":
+            print("Your vessel propels forth..")
+            print()
+        if player_choice == "f" and player_direction == "↑N":
             player_axis_y = player_axis_y + 1
             break
-        if direction == ("s"):
+        if player_choice == "f" and player_direction == "↓S":
             player_axis_y = player_axis_y - 1
             break
-        if direction == ("e"):
+        if player_choice == "f" and player_direction == "→E":
             player_axis_x = player_axis_x + 1
             break
-        if direction == ("w"):
+        if player_choice == "f" and player_direction == "←W":
             player_axis_x = player_axis_x - 1
-            break    
-        print("Invalid input..")
-        print()
+            break
+        if player_choice == "a":
+            print("Your vessel rises toward the surface..")
+            print()
+            player_axis_z = player_axis_z + 1
+            break
+        if player_choice == "d":
+            print("Your vessel lowers into the depths..")
+            print()
+            player_axis_z = player_axis_z - 1
+            break
+    # scanning
+        if player_choice == "s":
+            print("Target coordinance aquired! X:" + str(enemy_axis_x) + " Y:" + str(enemy_axis_y) + " Z:" + str(enemy_axis_z))
+            print()
+            break
+    # firing
+        if player_choice == "t":
+           print("Not yet implimented...")     
+           print()
+           break   
+    # error
+        else:
+            print("Invalid input..")
+            print()
+            
+
+        
 
 # game loop    
-
-def room_check():
-    global player_axis_y
-    global player_axis_x
-    if player_axis_x == 1 and player_axis_y == 5:
-        x1y5z1 = "X"
-   
-
+ 
 def game_loop():
+    print("╔═════════════════╗")
+    print("║ Submarine Game. ║")
+    print("╚═════════════════╝")
+    print()
     while True:
-        room_check()
-        map_display()
-        player_coordinates()
-        
-    
+        player_commands()
+      
+  
 # intitate game
 
 game_loop()
