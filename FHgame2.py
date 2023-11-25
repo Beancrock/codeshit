c7ea3490fde8c13799ad3da4b6afa52379594607
Beancrock <142068316+Beancrock@users.noreply.github.com>
2023-11-24 20:28:08 -0500

    Add files via upload

--- FHgame2.py
+++ FHgame2.py
@@ -1,57 +1,57 @@
-#imported shit
+# imported shit
 
 import random
 
-#default variables
+# default variables
 
 player_axis_y = 1
 player_axis_x = 3
 
-x1y5 = " "
-x1y4 = " "
-x1y3 = " "
-x1y2 = " "
-x1y1 = " "
-x2y5 = " "
-x2y4 = " "
-x2y3 = " "
-x2y2 = " "
-x2y1 = " "
-x3y5 = " "
-x3y4 = " "
-x3y3 = " "
-x3y2 = " "
-x3y1 = " "
-x4y5 = " "
-x4y4 = " "
-x4y3 = " "
-x4y2 = " "
-x4y1 = " "
-x5y5 = " "
-x5y4 = " "
-x5y3 = " "
-x5y2 = " "
-x5y1 = " "
+x1y5z1 = " "
+x1y4z1 = " "
+x1y3z1 = " "
+x1y2z1 = " "
+x1y1z1 = " "
+x2y5z1 = " "
+x2y4z1 = " "
+x2y3z1 = " "
+x2y2z1 = " "
+x2y1z1 = " "
+x3y5z1 = " "
+x3y4z1 = " "
+x3y3z1 = " "
+x3y2z1 = " "
+x3y1z1 = " "
+x4y5z1 = " "
+x4y4z1 = " "
+x4y3z1 = " "
+x4y2z1 = " "
+x4y1z1 = " "
+x5y5z1 = " "
+x5y4z1 = " "
+x5y3z1 = " "
+x5y2z1 = " "
+x5y1z1 = " "
 
 # map display
 
 def map_display():
-    print("[" + x1y5 + "][" + x2y5 + "][" + x3y5 + "][" + x4y5 + "][" + x5y5 + "]")
-    print("[" + x1y4 + "][" + x2y4 + "][" + x3y4 + "][" + x4y4 + "][" + x5y4 + "]")
-    print("[" + x1y3 + "][" + x2y3 + "][" + x3y3 + "][" + x4y3 + "][" + x5y3 + "]")
-    print("[" + x1y2 + "][" + x2y2 + "][" + x3y2 + "][" + x4y2 + "][" + x5y2 + "]")
-    print("[" + x1y1 + "][" + x2y1 + "][" + x3y1 + "][" + x4y1 + "][" + x5y1 + "]")
+    print("[" + x1y5z1 + "][" + x2y5z1 + "][" + x3y5z1 + "][" + x4y5z1 + "][" + x5y5z1 + "]")
+    print("[" + x1y4z1 + "][" + x2y4z1 + "][" + x3y4z1 + "][" + x4y4z1 + "][" + x5y4z1 + "]")
+    print("[" + x1y3z1 + "][" + x2y3z1 + "][" + x3y3z1 + "][" + x4y3z1 + "][" + x5y3z1 + "]")
+    print("[" + x1y2z1 + "][" + x2y2z1 + "][" + x3y2z1 + "][" + x4y2z1 + "][" + x5y2z1 + "]")
+    print("[" + x1y1z1 + "][" + x2y1z1 + "][" + x3y1z1 + "][" + x4y1z1 + "][" + x5y1z1 + "]")
     print()
 
 # player input
 
-def player_movement():
+def player_coordinates():
     global player_axis_y
     global player_axis_x
     print("Location: X=[" + str(player_axis_x) + "] Y=[" + str(player_axis_y) + "]")
     print()
     while True:
-        direction = input("(n)orth. (s)outh. (e)ast. (w)est. :")
+        direction = input("(N)orth. (S)outh. (E)ast. (W)est. :")
         print()
         if direction == ("n"):
             player_axis_y = player_axis_y + 1
@@ -60,24 +60,30 @@ def player_movement():
             player_axis_y = player_axis_y - 1
             break
         if direction == ("e"):
-            player_axis_x = player_axis_x - 1
+            player_axis_x = player_axis_x + 1
             break
         if direction == ("w"):
-            player_axis_x = player_axis_x + 1
+            player_axis_x = player_axis_x - 1
             break    
         print("Invalid input..")
         print()
 
-
 # game loop    
 
+def room_check():
+    global player_axis_y
+    global player_axis_x
+    if player_axis_x == 1 and player_axis_y == 5:
+        x1y5z1 = "X"
+   
 
 def game_loop():
     while True:
+        room_check()
         map_display()
-        player_movement()
+        player_coordinates()
         
-
+    
 # intitate game
 
 game_loop()
