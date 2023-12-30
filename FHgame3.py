36b166b338c3a3322b2f178bf6d579458ed9de97
Beancrock <142068316+Beancrock@users.noreply.github.com>
2023-12-30 14:36:51 -0500

    Add files via upload

--- FHgame3.py
+++ FHgame3.py
@@ -1,15 +1,22 @@
 '''
-╔╦╦╦╗
-╚╣╨╠╝
-╔╩╦╩╗
-╠╦╬╦╣
-╚╝╨╚╝
+
+╔═╦═╗
+╚╦╝╔╣
+╞╣╞╣║
+╔╩╗╚╣
+╚═╩═╝
+
+☻Ѫ
 '''
+
+import random
 
 # default values
 
 player_loc_x = 3
 player_loc_y = 3
+
+creature_01_location = "x3y4"
 
 x1y5 = " "
 x1y4 = " "
@@ -27,7 +34,7 @@ x3y5 = " "
 x3y4 = " "
 x3y3 = " "
 x3y2 = " "
-x3y1 = "☺"
+x3y1 = " "
 
 x4y5 = " "
 x4y4 = " "
@@ -41,9 +48,16 @@ x5y3 = " "
 x5y2 = " "
 x5y1 = " "
 
-# grid display
+# game loop
 
-def grid():
+def game_loop():
+    creature_01_movement()
+    creature_01_grid()
+    grid_display()
+
+# grid display
+    
+def grid_display():    
     global player_loc_x
     global player_loc_y
     
@@ -77,7 +91,6 @@ def grid():
     global x5y2
     global x5y1
     
-    print("X: "+ str(player_loc_x) + " Y:" + str(player_loc_y))
     print()
     print(x1y5 + x2y5 + x3y5 + x4y5 + x5y5)
     print(x1y4 + x2y4 + x3y4 + x4y4 + x5y4)
@@ -86,14 +99,252 @@ def grid():
     print(x1y1 + x2y1 + x3y1 + x4y1 + x5y1)
     print()
 
+# creature movement
+
+def creature_01_movement():
+    global creature_loc_x
+    global creature_loc_y
+    global creature_01_location
+    
+    global x1y5
+    global x1y4
+    global x1y3
+    global x1y2
+    global x1y1
+    
+    global x2y5
+    global x2y4
+    global x2y3
+    global x2y2
+    global x2y1
+    
+    global x3y5
+    global x3y4
+    global x3y3
+    global x3y2
+    global x3y1
+
+    global x4y5
+    global x4y4
+    global x4y3
+    global x4y2
+    global x4y1
+    
+    global x5y5
+    global x5y4
+    global x5y3
+    global x5y2
+    global x5y1
+
+    if creature_01_location == "x1y1":
+        x1y1 = "╚"
+        choices = ["x2y1", "x1y2"]
+        creature_01_location = random.choice(choices)
+    elif creature_01_location == "x2y1":
+        x2y1 = "╝"
+        choices = ["x1y1", "x2y2"]
+        creature_01_location = random.choice(choices)
+    elif creature_01_location == "x3y1":
+        x3y1 = "╨"
+        choices = ["x3y2"]
+        creature_01_location = random.choice(choices)
+    elif creature_01_location == "x4y1":
+        x4y1 = "╚"
+        choices = ["x4y2", "x5y1"]
+        creature_01_location = random.choice(choices)
+    elif creature_01_location == "x5y1":
+        x5y1 = "╝"
+        choices = ["x4y1", "x5y2"]
+        creature_01_location = random.choice(choices)
+
+    elif creature_01_location == "x1y2":
+        x1y2 = "╠"
+        choices = ["x1y1", "x2y2", "x1y3"]
+        creature_01_location = random.choice(choices)
+    elif creature_01_location == "x2y2":
+        x2y2 = "╦"
+        choices = ["x1y2", "x2y1", "x3y2"]
+        creature_01_location = random.choice(choices)
+    elif creature_01_location == "x3y2":
+        x3y2 = "╬"
+        choices = ["x2y2", "x3y3", "x3y1", "x4y2"]
+        creature_01_location = random.choice(choices)
+    elif creature_01_location == "x4y2":
+        x4y2 = "╦"
+        choices = ["x3y2", "x4y1", "x5y2"]
+        creature_01_location = random.choice(choices)
+    elif creature_01_location == "x5y2":
+        x5y2 = "╣"
+        choices = ["x4y2", "x5y1", "x5y3"]
+        creature_01_location = random.choice(choices)
+
+    elif creature_01_location == "x1y3":
+        x1y3 = "╔"
+        choices = ["x1y2", "x2y3"]
+        creature_01_location = random.choice(choices)
+    elif creature_01_location == "x2y3":
+        x2y3 = "╩"
+        choices = ["x1y3", "x2y4", "x3y3"]
+        creature_01_location = random.choice(choices)
+    elif creature_01_location == "x3y3":
+        x3y3 = "╦"
+        choices = ["x2y3", "x3y2", "x4y3"]
+        creature_01_location = random.choice(choices)
+    elif creature_01_location == "x4y3":
+        x4y3 = "╩"
+        choices = ["x3y3", "x4y4", "x5y3"]
+        creature_01_location = random.choice(choices)
+    elif creature_01_location == "x5y3":
+        x5y3 = "╗"
+        choices = ["x4y3", "x5y2"]
+        creature_01_location = random.choice(choices)
+
+    elif creature_01_location == "x1y4":
+        x1y4 = "╚"
+        choices = ["x1y5", "x2y4"]
+        creature_01_location = random.choice(choices)
+    elif creature_01_location == "x2y4":
+        x2y4 = "╣"
+        choices = ["x1y4", "x2y5", "x2y3"]
+        creature_01_location = random.choice(choices)
+    elif creature_01_location == "x3y4":
+        x3y4 = "╨"
+        choices = ["x3y5"]
+        creature_01_location = random.choice(choices)
+    elif creature_01_location == "x4y4":
+        x4y4 = "╠"
+        choices = ["x4y3", "x4y5", "x5y4"]
+        creature_01_location = random.choice(choices)
+    elif creature_01_location == "x5y4":
+        x5y4 = "╝"
+        choices = ["x4y4", "x5y5"]
+        creature_01_location = random.choice(choices)
+
+    elif creature_01_location == "x1y5":
+        x1y5 = "╔"
+        choices = ["x2y5", "x1y4"]
+        creature_01_location = random.choice(choices)
+    elif creature_01_location == "x2y5":
+        x2y5 = "╦"
+        choices = ["x1y5", "x2y4", "x3y5"]
+        creature_01_location = random.choice(choices)
+    elif creature_01_location == "x3y5":
+        x3y5 = "╦"
+        choices = ["x2y5", "x3y4", "x4y5"]
+        creature_01_location = random.choice(choices)
+    elif creature_01_location == "x4y5":
+        x4y5 = "╦"
+        choices = ["x3y5", "x4y4", "x5y5"]
+        creature_01_location = random.choice(choices)
+    elif creature_01_location == "x5y5":
+        x5y5 = "╗"
+        choices = ["x4y5", "x5y4"]
+        creature_01_location = random.choice(choices)                        
+
+# creature display
+
+def creature_01_grid():
+    global creature_01_location
+
+    global x1y5
+    global x1y4
+    global x1y3
+    global x1y2
+    global x1y1
+    
+    global x2y5
+    global x2y4
+    global x2y3
+    global x2y2
+    global x2y1
+    
+    global x3y5
+    global x3y4
+    global x3y3
+    global x3y2
+    global x3y1
+
+    global x4y5
+    global x4y4
+    global x4y3
+    global x4y2
+    global x4y1
+    
+    global x5y5
+    global x5y4
+    global x5y3
+    global x5y2
+    global x5y1
+     
+    if creature_01_location == "x1y1":
+        x1y1 = "Ѫ"
+    if creature_01_location == "x2y1":
+        x2y1 = "Ѫ"
+    if creature_01_location == "x3y1":
+        x3y1 = "Ѫ"
+    if creature_01_location == "x4y1":
+        x4y1 = "Ѫ"
+    if creature_01_location == "x5y1":
+        x5y1 = "Ѫ"    
+
+    if creature_01_location == "x1y2":
+        x1y2 = "Ѫ"
+    if creature_01_location == "x2y2":
+        x2y2 = "Ѫ"
+    if creature_01_location == "x3y2":
+        x3y2 = "Ѫ"
+    if creature_01_location == "x4y2":
+        x4y2 = "Ѫ"
+    if creature_01_location == "x5y2":
+        x5y2 = "Ѫ"        
+
+    if creature_01_location == "x1y3":
+        x1y3 = "Ѫ"
+    if creature_01_location == "x2y3":
+        x2y3 = "Ѫ"
+    if creature_01_location == "x3y3":
+        x3y3 = "Ѫ"
+    if creature_01_location == "x4y3":
+        x4y3 = "Ѫ"
+    if creature_01_location == "x5y3":
+        x5y3 = "Ѫ"    
+
+    if creature_01_location == "x1y4":
+        x1y4 = "Ѫ"
+    if creature_01_location == "x2y4":
+        x2y4 = "Ѫ"
+    if creature_01_location == "x3y4":
+        x3y4 = "Ѫ"
+    if creature_01_location == "x4y4":
+        x4y4 = "Ѫ"
+    if creature_01_location == "x5y4":
+        x5y4 = "Ѫ"
+
+    if creature_01_location == "x1y5":
+        x1y5 = "Ѫ"
+    if creature_01_location == "x2y5":
+        x2y5 = "Ѫ"
+    if creature_01_location == "x3y5":
+        x3y5 = "Ѫ"
+    if creature_01_location == "x4y5":
+        x4y5 = "Ѫ"
+    if creature_01_location == "x5y5":
+        x5y5 = "Ѫ"                            
+
+# collision check
+
+# "rooms"
+
 def room_x1y5():
     global player_loc_x
     global player_loc_y
     global x1y5
+    global player_location
+    player_location = "x1y5"
     player_loc_x = 1
     player_loc_y = 5
-    x1y5 = "☺"
-    grid()
+    x1y5 = "☻"
+    game_loop()
     x1y5 = "╔"
     while True:
         player_choice = input("(S)outh (E)ast :")
@@ -110,10 +361,12 @@ def room_x2y5():
     global player_loc_x
     global player_loc_y
     global x2y5
+    global player_location
+    player_location = "x2y5"
     player_loc_x = 2
     player_loc_y = 5
-    x2y5 = "☺"
-    grid()
+    x2y5 = "☻"
+    game_loop()
     x2y5 = "╦"
     while True:
         player_choice = input("(S)outh (E)ast (W)est :")
@@ -132,10 +385,12 @@ def room_x3y5():
     global player_loc_x
     global player_loc_y
     global x3y5
+    global player_location
+    player_location = "x3y5"
     player_loc_x = 3
     player_loc_y = 5
-    x3y5 = "☺"
-    grid()
+    x3y5 = "☻"
+    game_loop()
     x3y5 = "╦"
     while True:
         player_choice = input("(S)outh (E)ast (W)est :")
@@ -154,10 +409,12 @@ def room_x4y5():
     global player_loc_x
     global player_loc_y
     global x4y5
+    global player_location
+    player_location = "x4y5"
     player_loc_x = 4
     player_loc_y = 5
-    x4y5 = "☺"
-    grid()
+    x4y5 = "☻"
+    game_loop()
     x4y5 = "╦"
     while True:
         player_choice = input("(S)outh (E)ast (W)est :")
@@ -176,10 +433,12 @@ def room_x5y5():
     global player_loc_x
     global player_loc_y
     global x5y5
+    global player_location
+    player_location = "x5y5"
     player_loc_x = 5
     player_loc_y = 5
-    x5y5 = "☺"
-    grid()
+    x5y5 = "☻"
+    game_loop()
     x5y5 = "╗"
     while True:
         player_choice = input("(S)outh (W)est: ")
@@ -196,10 +455,12 @@ def room_x1y4():
     global player_loc_x
     global player_loc_y
     global x1y4
+    global player_location
+    player_location = "x1y4"
     player_loc_x = 1
     player_loc_y = 4
-    x1y4 = "☺"
-    grid()
+    x1y4 = "☻"
+    game_loop()
     x1y4 = "╚"
     while True:
         player_choice = input("(N)orth (E)ast :")
@@ -216,10 +477,12 @@ def room_x2y4():
     global player_loc_x
     global player_loc_y
     global x2y4
+    global player_location
+    player_location = "x2y4"
     player_loc_x = 2
     player_loc_y = 4
-    x2y4 = "☺"
-    grid()
+    x2y4 = "☻"
+    game_loop()
     x2y4 = "╣"
     while True:
         player_choice = input("(N)orth (S)outh (W)est :")
@@ -238,10 +501,12 @@ def room_x3y4():
     global player_loc_x
     global player_loc_y
     global x3y4
+    global player_location
+    player_location = "x3y4"
     player_loc_x = 3
     player_loc_y = 4
-    x3y4 = "☺"
-    grid()
+    x3y4 = "☻"
+    game_loop()
     x3y4 = "╨"
     while True:
         player_choice = input("(N)orth :")
@@ -256,10 +521,12 @@ def room_x4y4():
     global player_loc_x
     global player_loc_y
     global x4y4
+    global player_location
+    player_location = "x4y4"
     player_loc_x = 4
     player_loc_y = 4
-    x4y4 = "☺"
-    grid()
+    x4y4 = "☻"
+    game_loop()
     x4y4 = "╠"
     while True:
         player_choice = input("(N)orth (S)outh (E)ast :")
@@ -278,10 +545,12 @@ def room_x5y4():
     global player_loc_x
     global player_loc_y
     global x5y4
+    global player_location
+    player_location = "x5y4"
     player_loc_x = 5
     player_loc_y = 4
-    x5y4 = "☺"
-    grid()
+    x5y4 = "☻"
+    game_loop()
     x5y4 = "╝"
     while True:
         player_choice = input("(N)orth (W)est :")
@@ -298,10 +567,12 @@ def room_x1y3():
     global player_loc_x
     global player_loc_y
     global x1y3
+    global player_location
+    player_location = "x1y3"
     player_loc_x = 1
     player_loc_y = 3
-    x1y3 = "☺"
-    grid()
+    x1y3 = "☻"
+    game_loop()
     x1y3 = "╔"
     while True:
         player_choice = input("(S)outh (E)ast :")
@@ -318,10 +589,12 @@ def room_x2y3():
     global player_loc_x
     global player_loc_y
     global x2y3
+    global player_location
+    player_location = "x2y3"
     player_loc_x = 2
     player_loc_y = 3
-    x2y3 = "☺"
-    grid()
+    x2y3 = "☻"
+    game_loop()
     x2y3 = "╩"
     while True:
         player_choice = input("(N)orth (E)ast (W)est :")
@@ -340,10 +613,12 @@ def room_x3y3():
     global player_loc_x
     global player_loc_y
     global x3y3
+    global player_location
+    player_location = "x3y3"
     player_loc_x = 3
     player_loc_y = 3
-    x3y3 = "☺"
-    grid()
+    x3y3 = "☻"
+    game_loop()
     x3y3 = "╦"
     while True:
         player_choice = input("(S)outh (E)ast (W)est :")
@@ -362,10 +637,12 @@ def room_x4y3():
     global player_loc_x
     global player_loc_y
     global x4y3
+    global player_location
+    player_location = "x4y3"
     player_loc_x = 4
     player_loc_y = 3
-    x4y3 = "☺"
-    grid()
+    x4y3 = "☻"
+    game_loop()
     x4y3 = "╩"
     while True:
         player_choice = input("(N)orth (E)ast (W)est :")
@@ -384,10 +661,12 @@ def room_x5y3():
     global player_loc_x
     global player_loc_y
     global x5y3
+    global player_location
+    player_location = "x5y3"
     player_loc_x = 5
     player_loc_y = 3
-    x5y3 = "☺"
-    grid()
+    x5y3 = "☻"
+    game_loop()
     x5y3 = "╗"
     while True:
         player_choice = input("(S)outh (W)est :")
@@ -405,10 +684,12 @@ def room_x1y2():
     global player_loc_x
     global player_loc_y
     global x1y2
+    global player_location
+    player_location = "x1y2"
     player_loc_x = 1
     player_loc_y = 2
-    x1y2 = "☺"
-    grid()
+    x1y2 = "☻"
+    game_loop()
     x1y2 = "╠"
     while True:
         player_choice = input("(N)orth (S)outh (E)ast: ")
@@ -427,10 +708,12 @@ def room_x2y2():
     global player_loc_x
     global player_loc_y
     global x2y2
+    global player_location
+    player_location = "x2y2"
     player_loc_x = 2
     player_loc_y = 2
-    x2y2 = "☺"
-    grid()
+    x2y2 = "☻"
+    game_loop()
     x2y2 = "╦"
     while True:
         player_choice = input("(S)outh (E)ast (W)est :")
@@ -449,10 +732,12 @@ def room_x3y2():
     global player_loc_x
     global player_loc_y
     global x3y2
+    global player_location
+    player_location = "x3y2"
     player_loc_x = 3
     player_loc_y = 2
-    x3y2 = "☺"
-    grid()
+    x3y2 = "☻"
+    game_loop()
     x3y2 = "╬"
     while  True:
         player_choice = input("(N)orth (S)outh (E)ast (W)est :")
@@ -473,10 +758,12 @@ def room_x4y2():
     global player_loc_x
     global player_loc_y
     global x4y2
+    global player_location
+    player_location = "x4y2"
     player_loc_x = 4
     player_loc_y = 2
-    x4y2 = "☺"
-    grid()
+    x4y2 = "☻"
+    game_loop()
     x4y2 = "╦"
     while True:
         player_choice = input("(S)outh (E)ast (W)est :")
@@ -495,13 +782,15 @@ def room_x5y2():
     global player_loc_x
     global player_loc_y
     global x5y2
+    global player_location
+    player_location = "x5y2"
     player_loc_x = 5
     player_loc_y = 2
-    x5y2 = "☺"
-    grid()
+    x5y2 = "☻"
+    game_loop()
     x5y2 = "╣"
     while True:
-        player_choice = input("(N)orth (S)outh (E)ast :")
+        player_choice = input("(N)orth (S)outh (W)est :")
         print()
         if player_choice == "n":
             room_x5y3()
@@ -517,10 +806,12 @@ def room_x1y1():
     global player_loc_x
     global player_loc_y
     global x1y1
+    global player_location
+    player_location = "x1y1"
     player_loc_x = 1
     player_loc_y = 1
-    x1y1 = "☺"
-    grid()
+    x1y1 = "☻"
+    game_loop()
     x1y1 = "╚"
     while True:
         player_choice = input("(N)orth (E)ast :")
@@ -537,10 +828,12 @@ def room_x2y1():
     global player_loc_x
     global player_loc_y
     global x2y1
+    global player_location
+    player_location = "x2y1"
     player_loc_x = 2
     player_loc_y = 1
-    x2y1 = "☺"
-    grid()
+    x2y1 = "☻"
+    game_loop()
     x2y1 = "╝"
     while True:
         player_choice = input("(N)orth (W)est :")
@@ -557,10 +850,12 @@ def room_x3y1():
     global player_loc_x
     global player_loc_y
     global x3y1
+    global player_location
+    player_location = "x3y1"
     player_loc_x = 3
     player_loc_y = 1
-    x3y1 = "☺"
-    grid()
+    x3y1 = "☻"
+    game_loop()
     x3y1 = "╨"
     while True:
         player_choice = input("(N)orth :")
@@ -575,10 +870,12 @@ def room_x4y1():
     global player_loc_x
     global player_loc_y
     global x4y1
+    global player_location
+    player_location = "x4y1"
     player_loc_x = 4
     player_loc_y = 1
-    x4y1 = "☺"
-    grid()
+    x4y1 = "☻"
+    game_loop()
     x4y1 = "╚"
     while True:
         player_choice = input("(N)orth (E)ast :")
@@ -595,10 +892,12 @@ def room_x5y1():
     global player_loc_x
     global player_loc_y
     global x5y1
+    global player_location
+    player_location = "x5y1"
     player_loc_x = 5
     player_loc_y = 1
-    x5y1 = "☺"
-    grid()
+    x5y1 = "☻"
+    game_loop()
     x5y1 = "╝"
     while True:
         player_choice = input("(N)orth (W)est :")
@@ -610,5 +909,7 @@ def room_x5y1():
         else:
             print("Invalid input..")
             print()            
-           
+
+# initiate
+
 room_x3y1()
